a351137890be8c5331c2bb8ab2fd0bd4ecbebc4d
Jardel F. F. de Araujo <jardel.araujo@redeunifique.com.br>
2021-05-31 19:21:22 -0300

    ajustes na function getListRelative

--- compellent_colector/client.py
+++ compellent_colector/client.py
@@ -40,6 +40,7 @@ urllib3.disable_warnings()
 
 # Classe para pegar as métricas da Storage Dell Compellent através da REST API
 class Client(object):
+    # Inicializa o objeto da classe Client
     def __init__(self, host='192.168.110.10', username='', password='', port='3033'):
         self.api_base = 'api/rest'
         self.proto = 'https'
@@ -48,17 +49,10 @@ class Client(object):
         self.password = password if 'DELLSC_PASSWORD' not in os.environ else os.environ['DELLSC_PASSWORD']
         self.port = port if 'DELLSC_PASSWORD' not in os.environ else os.environ['DELLSC_PASSWORD']
         self.api_method = 'POST'
-        #self.api_login = '%s://%s:%s/%s/ApiConnection/Login' % (self.proto, self.host, self.port, self.api_base)
-        #self.api_login = '/ApiConnection/Login'
-        self.headers = {'x-dell-api-version': '5.2', 'Cookie': ''}
+        self.headers = {'x-dell-api-version': '5.2', 'Cookie': '', 'Content-Type': 'application/json'}
         self.login()
-        #self.res = requests.request(self.api_method, '%s' % self.api_login, headers=self.headers, verify=False, auth=HTTPBasicAuth(self.usename, self.password))
-        #if self.res.status_code == 200:
-        #    self.headers['Cookie'] = '%s=%s' % (self.res.cookies.keys()[0], self.res.cookies.values()[0])
-        #else:
-        #    print(self.res.text)
-        #    self.headers['Cookie'] = ''
 
+    # Método para fazer o login e armazenar o Cookie
     def login(self):
         self.api_method = 'POST'
         self.api_login = '%s://%s:%s/%s/ApiConnection/Login' % (self.proto, self.host, self.port, self.api_base)
@@ -70,23 +64,79 @@ class Client(object):
             self.headers['Cookie'] = ''
             return False
 
-    def _callRequest(self, url, method):
+    # Método interno para fazer a call das requests validando o retorno e fazendo o login caso necessário
+    def _callRequest(self, url, method, data=''):
         self.api_method = method
         api_url = '%s://%s:%s/%s%s' % (self.proto, self.host, self.port, self.api_base, url)
-        self.res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False)
+        self.data = data
+        if len(self.data) > 0:
+            self.res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False, data=self.data)
+        else:
+            self.res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False)
         if len(self.headers['Cookie']) == 0 or self.res.status_code == 401:
             self.login()
             return True
 
+    # Método para fazer o login e armazenar o Cookie
     def getListScAlerts(self, startTime, endTime, acknowledged=''):
         self.api_method = 'POST'
-        api_url = '/StorageCenter/ScAlert/GetList'
-        
+        api_url = '/StorageCenter/ScAlert/GetList'        
         self.res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False)
         self._callRequest(self.api_url, self.api_method)
         return pprint.pprint(self.res.json())
 
-    def getListRelative(self, api, period='5m', timezone='-03:00', acknowledged=''):
+    # Método para retornar um JSON com uma lista da request solicitada usando tempo relativo
+    def getListRelative(self, api, method='POST', period='5m', timezone='-03:00', acknowledged=''):
+        self.api = api
+        self.period = period
+        self.timezone = timezone
+        self.acknowledged = acknowledged
+        startTime = pandas.Timestamp('now', tz=self.timezone) - pandas.to_timedelta(self.period)
+        startTime = startTime.strftime('%Y-%m-%dT%H:%M:%S%Z')
+        self.api_method = method
+        self.api_url = '/StorageCenter/ScAlert/GetList'
+        self.filter_relative = """
+{
+    "Filter": {
+        "FilterType":"AND",
+        "Filters":[
+        {
+            "AttributeName":"createTime",
+            "AttributeValue":"%s",
+            "FilterType":"GreaterThan"
+        }
+        ]
+    }
+}
+        """ % re.sub('UTC', '', startTime)
+        self.filter_relative_ack = """
+{
+    "Filter": {
+        "FilterType":"AND",
+        "Filters":[
+            {
+                "AttributeName":"createTime",
+                "AttributeValue":"%s",
+                "FilterType":"GreaterThan"
+            },
+            {
+                "AttributeName":"acknowledged",
+                "AttributeValue":"%s",
+                "FilterType":"Equals"
+            }
+        ]
+    }
+}
+""" % (re.sub('UTC', '', startTime), self.acknowledged)
+        if len(self.acknowledged) > 0:
+            if re.match('true|True|false|False', self.acknowledged):
+                self._callRequest(self.api_url, self.api_method, data=self.filter_relative_ack)
+        else:
+            self._callRequest(self.api_url, self.api_method, data=self.filter_relative)
+        return self.res.json()
+
+    # Método para retornar um JSON com uma lista da request solicitada usando tempo absoluto
+    def getListAbsolute(self, api, method='POST', period='5m', timezone='-03:00', acknowledged=''):
         self.api = api
         self.period = period
         self.timezone = timezone
@@ -95,6 +145,7 @@ class Client(object):
         self.api_method = 'POST'
         self.api_url = '/StorageCenter/ScAlert/GetList'
         self._callRequest(self.api_url, self.api_method)
-        return re.sub('UTC', '', startTime)
+        #return re.sub('UTC', '', startTime)
+        return self.res.json()
 
 
