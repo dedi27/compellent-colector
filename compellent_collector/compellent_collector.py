c240d500e25988e23bf088b35cadd7672620ed1f
Jardel F. F. de Araujo <jardel.araujo@redeunifique.com.br>
2021-06-08 18:10:42 -0300

    ajustado saida do json

--- compellent_collector/compellent_collector.py
+++ compellent_collector/compellent_collector.py
@@ -31,10 +31,15 @@
 import sys
 import os
 import logging
+
 import compellent_collector.client as scc
 import argparse
+import json
+from pyutilsunifique import pyutils
+from requests.models import stream_decode_response_unicode
 from datetime import datetime
 from elasticsearch import Elasticsearch
+
 
 # Definições de log stdout
 stdout_log = logging.getLogger()
@@ -44,13 +49,21 @@ log_handler.setLevel(logging.INFO)
 log_handler.setFormatter(logging.Formatter('%(message)s'))
 stdout_log.addHandler(log_handler)
 
-
 def main():
     stdout_log.setLevel(logging.INFO)
     parser = argparse.ArgumentParser(prog='unifique-compellent-collector', description='Programa para coletar métricas da Storage Dell Compellent.')
     args = parser.parse_args()
-    teste = scc.Client(username='Admin', password='Dellsvcs1')
-    print(teste.getListScAlertsRelative(period='1d'))
+    dellSC = scc.Client(host='192.168.110.10', username='Admin', password='Dellsvcs1', verify_SSL=False)
+    raw_json = json.dumps(dellSC.getScServer(), indent=4)
+    stdout_log.info(pyutils.jsonPrint(raw_json, colorful=True))
+    raw_json = json.dumps(dellSC.getScCapabilities(), indent=4)
+    stdout_log.info(pyutils.jsonPrint(raw_json, colorful=True))  
+    raw_json = json.dumps(dellSC.getScConfiguration(), indent=4)
+    stdout_log.info(pyutils.jsonPrint(raw_json, colorful=True))
+
+    raw_json = json.dumps(dellSC.getListScChassisRelative(), indent=4)
+    stdout_log.info(pyutils.jsonPrint(raw_json, colorful=True))
+
 
 if __name__ == "main":
     main()
