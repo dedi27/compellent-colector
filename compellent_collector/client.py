1aa07b44b7a9af5c1378174c9a9fabb2e3187a08
Jardel F. F. de Araujo <jardel.araujo@redeunifique.com.br>
2021-06-08 18:08:00 -0300

    adicionado funções para retornar lista de servidores e de chassis.

--- compellent_collector/client.py
+++ compellent_collector/client.py
@@ -22,7 +22,7 @@
 
     Criado por: Jardel F. F. de Araujo
     Data de criação: 27/05/2021
-    Data de modificação: 06/06/2021
+    Data de modificação: 08/06/2021
     Versao: 0.1.0
 '''
 
@@ -38,36 +38,47 @@ from time import sleep, time
 
 urllib3.disable_warnings()
 
-# Classe para pegar as métricas da Storage Dell Compellent através da REST API
 class Client(object):
-    # Inicializa o objeto da classe Client
-    def __init__(self, host='192.168.110.10', username='', password='', protocol='https', verify_SSL=False,  port='3033', timezone='-03:00'):
+    """
+    Classe para pegar as métricas da Storage Dell Compellent através da REST API
+    """
+    def __init__(self, host, username, password, protocol='https', verify_SSL=True,  port='3033', timezone='-03:00'):
         """
-            >>> param timezone: Fusohorário da data (ex.: -03:00) 
+        Inicializa o objeto da classe Client
+        Parâmetros:
+            >>> param host: IP/Hostname do Dell Storage Center
+            >>> param username: Usuário do Dell Storage Center
+            >>> param password: Senha do Dell Storage Center
+            >>> param protocol: Protocolo da REST API do Dell Storage Center [HTTP|HTTPS] (Default: HTTPS)
+            >>> param verify_SSL: Ativar/Desativar verificação do SSL da REST API do Dell Storage Center (Default: True)
+            >>> param port: Dell Storage Center REST API port (Default: 3033)
+            >>> param timezone: Fusohorário da data (Default: -03:00) 
         """
         self.timezone = timezone
         self.api_base = 'api/rest'
-        self.proto = protocol
+        self.proto = protocol.lower()
         self.verify_SSL = verify_SSL
         self.host = host if 'DELLSC_HOST' not in os.environ else os.environ['DELLSC_HOST']
         self.usename = username if 'DELLSC_USERNAME' not in os.environ else os.environ['DELLSC_USERNAME']
         self.password = password if 'DELLSC_PASSWORD' not in os.environ else os.environ['DELLSC_PASSWORD']
         self.port = port if 'DELLSC_PASSWORD' not in os.environ else os.environ['DELLSC_PASSWORD']
-        self.api_method = 'POST'
+        api_method = 'POST'
         self.headers = {'x-dell-api-version': '5.2', 'Cookie': '', 'Content-Type': 'application/json'}
         self.login()
 
-    # Método para fazer o login e armazenar o Cookie
     def login(self):
+        """
+        Função para fazer o login e armazenar o Cookie
+        """
         if self._isClientLogged():
             return  True
         else:
-            self.api_method = 'POST'
+            api_method = 'POST'
             api_login = '%s://%s:%s/%s/ApiConnection/Login' % (self.proto, self.host, self.port, self.api_base)
             if self.proto == 'https':
-                self.res = requests.request(self.api_method, '%s' % api_login, headers=self.headers, verify=self.verify_SSL, auth=HTTPBasicAuth(self.usename, self.password))
+                self.res = requests.request(api_method, '%s' % api_login, headers=self.headers, verify=self.verify_SSL, auth=HTTPBasicAuth(self.usename, self.password))
             else:
-                self.res = requests.request(self.api_method, '%s' % api_login, headers=self.headers, auth=HTTPBasicAuth(self.usename, self.password))
+                self.res = requests.request(api_method, '%s' % api_login, headers=self.headers, auth=HTTPBasicAuth(self.usename, self.password))
             if self.res.status_code == 200:
                 self.headers['Cookie'] = '%s=%s' % (self.res.cookies.keys()[0], self.res.cookies.values()[0])
                 return True
@@ -87,19 +98,27 @@ class Client(object):
         else:
             return True
 
-    # Método interno para fazer a call das requests validando o retorno e fazendo o login caso necessário
-    def _apiRequest(self, url, data=''):
+    def _apiRequest(self, url, method, data=''):
+        """
+        Função interna para fazer a call das requests validando o retorno e fazendo o login caso necessário
+        """
         api_url = '%s://%s:%s/%s%s' % (self.proto, self.host, self.port, self.api_base, url)
         retry = 0
         while not self.login() or retry < 5:
             retry += 1
             sleep(0.3)
-        if len(data) > 0:
-            res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False, data=data)
-            return res.json()
         else:
-            res = requests.request(self.api_method, '%s' % api_url, headers=self.headers, verify=False)
-            return res.json()
+            if self.login():
+                if len(data) > 0:
+                    res = requests.request(method, '%s' % api_url, headers=self.headers, verify=False, data=data)
+                else:
+                    res = requests.request(method, '%s' % api_url, headers=self.headers, verify=False)
+                if self.res.status_code == 200:
+                    return res.json()
+                else:
+                    return {}
+            
+
 
     def _getTimeListRelative(self, api, period, acknowledged):
         """
@@ -114,7 +133,7 @@ class Client(object):
         self.startTime = pandas.Timestamp('now', tz=self.timezone) - pandas.to_timedelta(self.period)
         self.startTime = self.startTime.strftime('%Y-%m-%dT%H:%M:%S%Z')
         self.api_url = api
-        self.api_method = 'POST'
+        api_method = 'POST'
         filter_relative = """
 {
     "Filter": {
@@ -151,10 +170,10 @@ class Client(object):
         if len(acknowledged) > 0:
             if re.match('true|True|false|False', acknowledged):
                 #self._apiRequest(self.api_url, data=filter_relative_ack)
-                return sorted(self._apiRequest(self.api_url, data=filter_relative_ack), key=lambda x: x['createTime'])
+                return sorted(self._apiRequest(self.api_url, method=api_method, data=filter_relative_ack), key=lambda x: x['createTime'])
         else:
             #self._apiRequest(self.api_url, data=filter_relative)
-            return sorted(self._apiRequest(self.api_url, data=filter_relative), key=lambda x: x['createTime'])
+            return sorted(self._apiRequest(self.api_url, method=api_method, data=filter_relative), key=lambda x: x['createTime'])
         #return sorted(self.res.json(), key=lambda x: x['createTime'])
 
     def _getTimeListAbsolute(self, api, startTime, endTime, acknowledged):
@@ -168,9 +187,9 @@ class Client(object):
         """
         startTime = startTime + self.timezone
         endTime = endTime + self.timezone
-        self.api_method = 'POST'
+        api_method = 'POST'
         self.api_url = api
-        filter_relative = """
+        filter_absolute = """
 {
     "Filter": {
         "FilterType":"AND",
@@ -189,7 +208,7 @@ class Client(object):
     }
 }
         """ % (startTime, endTime)
-        filter_relative_ack = """
+        filter_absolute_ack = """
 {
     "Filter": {
         "FilterType":"AND",
@@ -215,12 +234,9 @@ class Client(object):
 """ % (startTime, endTime, acknowledged)
         if len(acknowledged) > 0:
             if re.match('true|True|false|False', acknowledged):
-                #self._apiRequest(self.api_url, data=filter_relative_ack)
-                return sorted(self._apiRequest(self.api_url, data=filter_relative_ack), key=lambda x: x['createTime'])
+                return sorted(self._apiRequest(self.api_url, method=api_method, data=filter_absolute_ack), key=lambda x: x['createTime'])
         else:
-            #self._apiRequest(self.api_url, data=filter_relative)
-            return sorted(self.res.json(), key=lambda x: x['createTime'])
-        #return sorted(self._apiRequest(self.api_url, data=filter_relative), key=lambda x: x['createTime'])
+            return sorted(self._apiRequest(self.api_url, method=api_method, data=filter_absolute), key=lambda x: x['createTime'])
 
     def getListScAlertsRelative(self, period='5m', acknowledged=''):
         """
@@ -228,7 +244,6 @@ class Client(object):
         """
         api_url = '/StorageCenter/ScAlert/GetList'        
         return self._getTimeListRelative(api_url, period, acknowledged)
-        #return self.res.json()
 
     def getListScAlertsAbsolute(self, startTime, endTime, acknowledged=''):
         """"
@@ -240,19 +255,34 @@ class Client(object):
         """
         api_url = '/StorageCenter/ScAlert/GetList'        
         return self._getTimeListAbsolute(api_url, startTime, endTime, acknowledged)
-        #return self.res.json()
 
     def getScCapabilities(self):
         """"
         Esta função retorna um json com a lista de capacidades da Storage
         """
         api_url = '/StorageCenter/ScCapabilities/GetList'
-        self.api_method = 'POST'
-        return self._apiRequest(api_url)
+        api_method = 'POST'
+        return self._apiRequest(api_url, method=api_method)
 
     def getScConfiguration(self):
         """"
         Esta função retorna um json com a lista de configurações da Storage
         """
         api_url = '/StorageCenter/ScConfiguration'
-        self.api_method = 'GET'
+        api_method = 'GET'
+        return self._apiRequest(api_url, api_method) 
+
+    def getScServer(self):
+        """"
+        Esta função retorna um json com a lista de Servervidores conectador na Storage
+        """
+        api_url = '/StorageCenter/ScServer'
+        api_method = 'GET'
+        return self._apiRequest(api_url, api_method)    
+
+    def getListScChassis(self, period='5m', acknowledged=''):
+        """
+        Esta função retorna um json com a lista de Chassis da Storage usando formato de tempo relativo
+        """
+        api_url = '/StorageCenter/ScChassis/GetList'        
+        return self._getTimeListRelative(api_url, period, acknowledged)
